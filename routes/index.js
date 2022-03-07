e827b38554b8678298e7d06f8d4d15d1142728a8
Pranjay <pranjay.saini2@gmail.com>
2022-03-08 00:32:38 +0530

    wild card route

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 const routeUser = require('./routeUser');
 
-
-
-
 router.use('/user', routeUser);
 
+router.use('/', (req, res, next) => {
+  res.status(200).render('Hello World');
+});
 
 module.exports = router;
